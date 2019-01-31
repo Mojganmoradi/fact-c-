7b89619617750882f9ea3269ed2edb00af15f02c
mojgan moradi <46632304+Mojganmoradi@users.noreply.github.com>
2019-02-01 02:45:22 +0330

    Add files via upload

---
+++ Array Exam.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	
+	int a;
+	int array[5];
+	cout<<"please enter 5 number:";
+	
+	for(int i=0; i<5; i++){
+		cin>>array[i];
+	
+	}
+     	cout<<"++++++";
+
+	for(int j=0; j<5; j++){
+	cout<<array[j];
+	}
+	
+}
